acf1f71c652b9ff5eb620a20f939951b2e026474
juancamilo123456 <165239968+juancamilo123456@users.noreply.github.com>
2024-04-16 23:42:50 -0500

    Update settings.py

--- Proyecto_Django_parcial/settings.py
+++ Proyecto_Django_parcial/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
